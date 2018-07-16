6a54926e588a6396c8dd3a2835b69e0bff351117
techdem <tudor.chiribes@gmail.com>
2018-07-16 12:15:15 +0100

    added code for final question

---
+++ src/declarations_and_access_control/MyQuestion5.java
@@ -0,0 +1,19 @@
+interface MyQuestionS {
+	static void MyQuestionS() {
+		final int MyQuestionS;
+	}
+	{ MyQuestionS = 5; }
+}
+public class MyQuestion5 implements MyQuestionS {
+	static int MyQuestion5;
+	public static void main(String[] args) {
+		int MyQuestion5;
+	}
+	{ MyQuestion5 = 5; MyQuestionS.MyQuestionS(); }
+	MyQuestion5(int MyQuestion5) {
+		this();
+	}
+	MyQuestion5() {
+		new MyQuestion5().MyQuestion5 = 5;
+	}
+}
