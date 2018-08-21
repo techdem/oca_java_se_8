a2c2211cec3033f5a2f8fc2315d10cdc9a669d4d
techdem <tudor.chiribes@gmail.com>
2018-08-21 15:44:38 +0100

    p.253

---
+++ src/operators/TestOR.java
@@ -0,0 +1,22 @@
+package operators;
+
+class TestOR {
+    public static void main(String[] args) {
+        if ((isItSmall(3)) || (isItSmall(7))) {
+            System.out.println("Result is true");
+        }
+        if ((isItSmall(6)) || (isItSmall(9))) {
+            System.out.println("Result is true");
+        }
+    }
+
+    public static boolean isItSmall(int i) {
+        if (i < 5) {
+            System.out.println("i < 5");
+            return true;
+        } else {
+            System.out.println("i >= 5");
+            return false;
+        }
+    }
+}
