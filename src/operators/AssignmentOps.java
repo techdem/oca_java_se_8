e698a3d9afb4bc2dd0db79b23c9ba83803cb7e13
techdem <tudor.chiribes@gmail.com>
2018-08-21 15:40:05 +0100

    p.251

---
+++ src/operators/AssignmentOps.java
@@ -0,0 +1,12 @@
+package operators;
+
+class AssignmentOps {
+    public static void main(String [] args) {
+        int sizeOfYard = 10;
+        int numOfPets = 3;
+        String status = (numOfPets<4)?"Pet count OK"
+            :(sizeOfYard > 8)? "Pet limit on the edge"
+                :"too many pets";
+        System.out.println("Pet status is " + status);
+    }
+}
