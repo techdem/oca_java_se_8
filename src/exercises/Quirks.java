ccbc16a45d35819aa0c4c3740cc6007a559af4b3
techdem <tudor.chiribes@gmail.com>
2018-10-14 02:26:05 +0100

    added quirks

---
+++ src/exercises/Quirks.java
@@ -0,0 +1,33 @@
+package exercises;
+
+//class Nul {
+//    static String nullString;
+//    public static void main(String[] args) {
+//        StringBuilder stringBuilder = new StringBuilder();
+//        stringBuilder.append(nullString, 0, 3);
+//        System.out.println(stringBuilder);
+//    }
+//}
+
+//class Scoop {
+//    static int thrower() throws Exception { return 42; }
+//    public static void main(String[] args) {
+//        int x = 0;
+//        try {
+//            x = thrower();
+//        } catch (Exception e) {
+//            x++;
+//        } finally {
+//            System.out.println("x = " +  ++x);
+//        }
+//    }
+//}
+
+class Vark {
+    public static void main(String... a) {
+        new Vark().go(a, 42);
+    }
+    void go(String[] b, int life) {
+        System.out.println(b[1]);
+    }
+}
