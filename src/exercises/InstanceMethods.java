3cdc26900687281ce7a6fc70abf9d47645a16009
techdem <tudor.chiribes@gmail.com>
2018-10-16 18:09:07 +0100

    added interfaces to polymorphism

--- src/exercises/InstanceMethods.java
+++ src/exercises/InstanceMethods.java
@@ -1,37 +1,87 @@
 package exercises;
 
-class SuperClass {
+interface SuperInterface {
     static int staticNumber = 1;
     int instanceNumber = 2;
 
-    static void staticMethod() {
-        System.out.println("static super method");
-    }
+    static void staticMethod() { System.out.println("super interface static  method"); }
+    default void instanceMethod() { System.out.println("super instance super method"); };
+}
 
-    void instanceMethod() {
-        System.out.println("instance super method");
+interface SubInterface extends SuperInterface {
+    static int staticNumber = 3;
+    int instanceNumber = 4;
+
+    static void staticMethod() { System.out.println("sub interface static  method"); }
+    default void instanceMethod() { System.out.println("sub instance super method"); };
+}
+
+class SuperClass implements SuperInterface {
+    static int staticNumber = 5;
+    int instanceNumber = 6;
+
+    static void staticMethod() {
+        System.out.println("super class static method");
+    }
+    public void instanceMethod() {
+        System.out.println("super class instance method");
     }
 }
 
-class SubClass extends SuperClass {
-    static int staticNumber = 2;
-    int instanceNumber = 3;
+class SubClass extends SuperClass implements SubInterface {
+    static int staticNumber = 7;
+    int instanceNumber = 8;
 
     static void staticMethod() {
-        System.out.println("static sub method");
+        System.out.println("sub class static method");
     }
-
-    void instanceMethod() {
-        System.out.println("instance sub method");
+    public void instanceMethod() {
+        System.out.println("sub class instance method");
     }
 }
 
 public class InstanceMethods {
     public static void main(String[] args) {
-        SuperClass sc = new SubClass();
-        System.out.println(sc.staticNumber);
-        System.out.println(sc.instanceNumber);
-        sc.staticMethod();
-        sc.instanceMethod();
+        //SuperInterface superInterface = new SuperInterface(); // cannot instantiate abstract
+        SuperInterface superInterfaceSuperClass = new SuperClass();
+        SuperInterface superInterfaceSubClass = new SubClass();
+        //SubInterface subInterfaceSuperClass = new SuperClass(); // SuperClass is not a SubInterface
+        SubInterface subInterfaceSubClass = new SubClass();
+
+        SuperClass superClassSuperClass = new SuperClass();
+        SuperClass superClassSubClass = new SubClass();
+        //SubClass subClassSuperClass = new SuperClass(); // SuperClass is not a SubClass
+        SubClass subClassSubClass = new SubClass();
+
+        System.out.println(superInterfaceSuperClass.staticNumber);
+        System.out.println(superInterfaceSuperClass.instanceNumber);
+        //superInterfaceSuperClass.staticMethod(); // cannot invoke interface static method through instance variable
+        SuperClass.staticMethod();
+        superInterfaceSuperClass.instanceMethod();
+
+        System.out.println(superInterfaceSubClass.staticNumber);
+        System.out.println(superInterfaceSubClass.instanceNumber);
+        //superInterfaceSubClass.staticMethod(); // cannot invoke interface static method through instance variable
+        superInterfaceSubClass.instanceMethod();
+
+        System.out.println(subInterfaceSubClass.staticNumber);
+        System.out.println(subInterfaceSubClass.instanceNumber);
+        //subInterfaceSubClass.staticMethod(); // cannot invoke interface static method through instance variable
+        superInterfaceSubClass.instanceMethod();
+
+        System.out.println(superClassSuperClass.staticNumber);
+        System.out.println(superClassSuperClass.instanceNumber);
+        superClassSuperClass.staticMethod();
+        superClassSuperClass.instanceMethod();
+
+        System.out.println(superClassSubClass.staticNumber);
+        System.out.println(superClassSubClass.instanceNumber);
+        superClassSubClass.staticMethod();
+        superClassSubClass.instanceMethod();
+
+        System.out.println(subClassSubClass.staticNumber);
+        System.out.println(subClassSubClass.instanceNumber);
+        subClassSubClass.staticMethod();
+        subClassSubClass.instanceMethod();
     }
 }
